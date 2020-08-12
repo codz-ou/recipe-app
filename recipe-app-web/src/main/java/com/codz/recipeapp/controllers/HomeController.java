cb1efe14a711ab59cc87ee1a29dbed922cd8b18a
codz-ou <codz.ou@gmail.com>
2020-08-12 17:28:51 +0300

    Seperated project into web and data modules. Migrated dependencies. Closes #1

---
+++ recipe-app-web/src/main/java/com/codz/recipeapp/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.codz.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    private String homePage(){
+        return "index";
+    }
+
+}
