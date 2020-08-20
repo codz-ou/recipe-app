ab63c69faf615d634fe02f9eb683a0e16d6cde42
codz-ou <codz.ou@gmail.com>
2020-08-20 06:44:34 +0300

    added 1=1relationship between Ingredients and UnitOfMeasure.  Closes #6

--- recipe-app-data/src/main/java/com/codz/retipeapp/model/Ingredient.java
+++ recipe-app-data/src/main/java/com/codz/retipeapp/model/Ingredient.java
@@ -11,10 +11,19 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private unitOfMeasure unitOfMeasure
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
 
     @ManyToOne
     private Recipe recipe;
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
 
     public Long getId() {
         return id;
